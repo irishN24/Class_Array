fc6d429c514e9588940af72e7e710cf9bf928db2
irishN24 <145177386+irishN24@users.noreply.github.com>
2024-11-28 12:08:20 +0700

    Update Class_Array.cpp

--- Class_Array.cpp
+++ Class_Array.cpp
@@ -1,6 +1,7 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cstring>
 #include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 
@@ -8,8 +9,8 @@ class Array {
     int *a, n;
 
 public:
-    Array(int len = 1, int t = 1, int min_d = 0, int max_d = 50); // конструктор 1
-    Array(int* arr, int l); // конструктор 2
+    Array(size_t len = 1, int t = 1, int min_d = 0, int max_d = 10); // конструктор 1 ++
+    Array(int* arr, size_t l); // конструктор 2 ++
     Array(Array& arr); //конструктор копирования ++
     ~Array() { //деструктор ++
         if (a) {
@@ -17,10 +18,10 @@ public:
             a = NULL;
         }
     }
-    Array& operator =(Array& arr); //оператор присвоения
-    int& operator [](int i);// оператор взятия индекса
-    bool Test(); // проверка на неупорядоченность
-    bool operator == (Array& arr); // оператор равенства
+    Array& operator =(Array& arr); //оператор присвоения ++
+    int& operator [](int i);// оператор взятия индекса ++
+    bool Test(); // проверка на упорядоченность++
+    bool operator == (Array arr); // оператор равенства недоделана
     void Shell_sort(); //сортировка Шелла
     void HeapSort(); //Пирамидальная сортировка
     void Hoar_Sort(); // Сортировка Хоара
@@ -28,13 +29,14 @@ public:
     friend istream& operator >> (istream& is, Array& a); //оператор ввод ++
     friend ostream& operator << (ostream& os, Array& a); //оператор вывод ++
 };
-Array::Array(int len, int t, int min_d, int max_d) {
-    if (len > 0) { // проверка длины массива 
+//конструктор 1
+Array::Array(size_t len, int t, int min_d, int max_d) {
+    if (len > 0) { // проверка длины массива
         if (max_d > min_d) { //проверка правильного диапазона
-            a = new int[len]; //выделяем память 
+            a = new int[len]; //выделяем память
             if (a != nullptr) {
                 this->n = len; // Инициализируем длину
-                srand(time(NULL)); 
+                srand(time(NULL));
                 if (t == 1) {
                     for (int i = 0; i < len; i++) {
                         a[i] = min_d + rand() % (max_d - min_d + 1);
@@ -42,13 +44,13 @@ Array::Array(int len, int t, int min_d, int max_d) {
                 }
                 else if (t == 2) {
                     a[0] = min_d + rand() % max_d; // первый элемент >= мин
-                    for (int i = 0; i < len; i++) {
+                    for (int i = 1; i < len; i++) {
                         a[i] = a[i - 1] + rand() % max_d; // каждый следующий >= предыдущего
                     }
                 }
                 else if (t == 3) {
                     a[0] = max_d - rand() % min_d; // первый элемент <= max
-                    for (int i = 0; i < len; i++) {
+                    for (int i = 1; i < len; i++) {
                         a[i] = a[i - 1] - rand() % min_d; // каждый следующий <= предыдущего
                     }
                 }
@@ -62,7 +64,19 @@ Array::Array(int len, int t, int min_d, int max_d) {
         }
     }
 }
-
+//конструктор 2
+Array:: Array(int *arr, size_t l){
+    if(arr != nullptr){
+        a = new int[l];
+        if(a != nullptr){
+            this -> n = l;
+            for(int i = 0; i < l; i++){
+                a[i] = arr[i];
+            }
+        }
+    }
+}
+//конструктор копированипя
 Array::Array(Array &arr) {
     n = arr.n;
     a = new int[n];
@@ -72,7 +86,63 @@ Array::Array(Array &arr) {
         }
     }
 }
-istream& operator>>(istream& is, Array &a) { //дружественный ввод
+//оператор присвоения
+Array &Array::operator =(Array& arr){
+    if(this != &arr){
+        delete[] a;
+        n = arr.n;
+        a = new int[n];
+        if(a){
+            for(int i = 0; i < n; i++){
+                a[i] = arr[i];
+            }
+        }
+    }
+    return *this;
+}
+//взятие индекса
+int &Array::operator[](int i){
+    if(i < 0 || i < n){
+        std::cout << "\nIndex outside the array!!!" << "\n";
+        return a[0];
+    }
+    return a[i];
+}
+//проверка на неупорядоченность
+bool Array::Test(){
+    for(int i = 0; i < n - 1;i++){
+        if(a[i] > a[i+1]){
+            return false;
+        }
+    }
+    return true;
+}
+//оператор равенства
+bool Array::operator == (Array arr){//массивы равны если у них одинаковые элементы
+    if(n != arr.n){
+        return false;
+    }
+    int i = 0;
+    while(i < arr.n){
+        bool flag = false;
+        for (int j = 0; j < n; j++){
+            if(arr.a[i] == a[j]){
+                arr.a[i] = arr.a[arr.n - 1];
+                arr.n--;
+                flag = true;
+                break;
+            }
+        }
+        if(!flag){
+            return false;
+        }
+        i++;
+    }
+    return true;
+}
+
+
+/*istream& operator>>(istream& is, Array &a) { //дружественный ввод
     int len;
     cout << "Enter the size of the array: ";
     cin >> len;
@@ -97,11 +167,10 @@ istream& operator>>(istream& is, Array &a) { //дружественный вво
                 is >> a[i];
             }
         }
-        return is;
     }
-
+    return is;
 }
-
+*/
 ostream &operator<<(ostream &os, Array& a) { //дружественный вывод
     for (int i = 0; i < a.n; i++) {
         os << a.a[i] << " ";
@@ -112,9 +181,12 @@ ostream &operator<<(ostream &os, Array& a) { //дружественный выв
 
 int main()
 {
-    Array Array1(int len, int t, int min_d, int max_d);
-    cout << "Array 1: " << Array1 << "\n";
+    Array Array1(200, 1, 0, 50);
+    cout << "Array 1:{ " << Array1 << "\n";
+    cout << "==========================" << "\n";
+    int arrr[5] = {2,4,7,90,45};
+    Array Array2(arrr, 5);
+    cout << "Array 2: " << Array2 << "\n";
     cout << "==========================" << "\n";
 
-    
 }
